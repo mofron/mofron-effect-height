2e45ab968b880dba2627847cc0c6155a3a630212
simpart <taria.ki4m@gmail.com>
2022-05-22 01:18:46 +0000

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# mofron-effect-fadepack
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+fade-in,fade-out effect for mofron input component
+
+
+# Install
+```
+npm install mofron mofron-effect-fadepack
+```
+
+# Parameter
+
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+| | height | size | height size of before effect |
+| | | size | height size of after effect |
+| | fromHeight | size | height size of before effect |
+| | toHeight | size | height size of after effect |
+
---
+++ index.js
@@ -0,0 +1,116 @@
+/**
+ * @file mofron-effect-fadepack/index.js
+ * @brief fade-in,fade-out effect for mofron input component
+ * @license MIT
+ */
+const Fade = require("mofron-effect-fade");
+
+module.exports = class extends mofron.class.Effect {
+    /**
+     * initialize effect
+     * 
+     * @param (mixed) 
+     *                key-value: effect config
+     * @short short
+     * @type private
+     */
+    constructor (p1) {
+        try {
+            super();
+            this.modname("Height");
+            this.shortForm("height");
+            
+            this.confmng().add("fromHeight", { type: "size" });
+            this.confmng().add("toHeight",   { type: "size" });
+            
+	    this.speed(500);
+            this.transition("height");
+            
+            this.beforeEvent(
+                (eff) => {
+                    try {
+                        let hei = eff.confmng("fronHeight");
+                        eff.component().style({
+                            "height" : (null === hei) ? undefined : hei
+                        });
+		    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                }
+            );
+
+            /* init config */
+	    if (0 < arguments.length) {
+                this.config(p1);
+	    }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * effect contents
+     * @type private
+     */
+    contents (cmp) {
+        try {
+            cmp.height(this.toHeight());
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * height getter/setter
+     *
+     * @param (size) height size of before effect
+     * @param (size) height size of after effect
+     * @return (size) height size of after effect
+     * @type parameter
+     */
+    height (from, to) {
+        try {
+            this.fromHeight(from);
+            return this.toHeight(to);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * height getter/setter
+     *
+     * @param (size) height size of before effect
+     * @return (size) height size of before effect
+     * @type parameter
+     */
+    fromHeight (prm) {
+        try {
+            return this.confmng("fromHeight", prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * height setter/getter
+     * 
+     * @param (size) height size of after effect
+     * @return (size) height size of after effect
+     * @type parameter
+     */
+    toHeight (prm) {
+        try {
+            return this.confmng("toHeight", prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+/* end of file */
